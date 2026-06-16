d4cc11558850b052899ecc3668e60491f7ee51cc
unknown <sanjayparasuraman90@gmail.com>
2026-06-16 20:43:46 +0530

    Initailized config db server code

---
+++ Backend/config/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect('mongodb://127.0.0.1:27017/ecommerce_simple');
+        console.log("MongoDB Connected Successfully");
+    } catch (err) {
+        console.error("DB Connection Error:", err);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
